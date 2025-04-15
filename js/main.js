90fe30fed1cab8a60739f92804391eb1806bc8f9
barboskin-a <lizik.rabotyga45@gmail.com>
2025-04-15 21:10:14 +0700

    add methods in app

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ let app = new Vue({
     el: '#app',
     data() {
         return {
+            newCard: {
+                title: '',
+                list: ['', '', ''],
+                completedAt: null,
+            },
             columns: [
                 {cards: JSON.parse(localStorage.getItem('column1')) || []}, /*парсим строки в объекты*/
                 {cards: JSON.parse(localStorage.getItem('column2')) || []},
                 {cards: JSON.parse(localStorage.getItem('column3')) || []},
             ],
+            blockFirstColumn: false,
         };
     },
     components: {
@@ -26,7 +32,67 @@ let app = new Vue({
             },
         },
         methods: {
-
+            methods: {
+                updateCard(index, column, data) {
+                    Vue.set(this.columns[column - 1].cards[index], 'completedAt', data.completedAt);
+                    this.saveData();
+                },
+                moveCard(cardIndex, columnIndex) {
+                    const card = this.columns[cardIndex.column - 1].cards.splice(cardIndex.index, 1)[0];
+                    this.columns[columnIndex - 1].cards.push(card);
+                    this.saveData();
+                    this.checkBlockFirstColumn();
+                },
+                checkBlockFirstColumn() {
+                    const secondColumnFull = this.columns[1].cards.length >= 5;
+                    const firstColumnHasProgressingCard = this.columns[0].cards.some(card => {
+                        const completedItems = card.list.filter(item => item.done).length;
+                        return Math.floor((completedItems / card.list.length) * 100) > 50;
+                    });
+                    if (secondColumnFull && firstColumnHasProgressingCard) {
+                        this.blockFirstColumn = true;
+                    } else if (this.columns[1].cards.length < 5) {
+                        this.blockFirstColumn = false;
+                    }
+                },
+                saveData() {
+                    localStorage.setItem('column1', JSON.stringify(this.columns[0].cards));
+                    localStorage.setItem('column2', JSON.stringify(this.columns[1].cards));
+                    localStorage.setItem('column3', JSON.stringify(this.columns[2].cards));
+                },
+                addItem() {
+                    if (this.newCard.list.length < 5) {
+                        this.newCard.list.push('');
+                    }
+                },
+                removeItem(index) {
+                    if (this.newCard.list.length > 3) {
+                        this.newCard.list.splice(index, 1);
+                    }
+                },
+                addNewCard() {
+                    if (this.blockFirstColumn) {
+                        alert('Первый столбец заблокирован, пока во втором не появится свободное место');
+                        return;
+                    }
+                    if (this.newCard.title.trim() && this.newCard.list.every(item => item.trim())) {
+                        const newCard = {
+                            title: this.newCard.title,
+                            list: this.newCard.list.map(text => ({ text, done: false })),
+                        };
+                        if (this.columns[0].cards.length < 3) {
+                            this.columns[0].cards.push(newCard);
+                            console.log('New card added:', newCard);
+                        } else {
+                            alert('В первом храниться не более 3-х карточек!');
+                        }
+                        this.saveData();
+                        this.newCard = { title: '', list: ['', '', '', ''] };
+                    } else {
+                        alert('Введите заголовок и минимум 3 пункта!');
+                    }
+                },
+            },
         },
         template: `
                  <div class="card">
